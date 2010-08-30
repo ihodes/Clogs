0968e83e060368955124510ae838a78556111e18
Isaac Hodes <ihodes@mac.com>
2010-08-30 17:48:12 -0400

    made it possible to replace the meta of an old post.md to an updated version, and also implemented some dates.clj related changes

--- src/clogs/processor.clj
+++ src/clogs/processor.clj
@@ -1,5 +1,6 @@
 (ns clogs.processor
   (require [clogs.render :as r]
+           [clogs.dates :as d]
            [clojure.java.io :as io]
            [clojure.string :as s]))
 
@@ -9,14 +10,14 @@
 
 (defn extract-post-meta
   "Returns the map found on the first line of the post.md file.
-   'postdir is a directory that countains a post.md file.
+   'postdir is a directory that contains a post.md file.
 
    e.g. resources/p/test/"
   [postdir]
   (with-open [f (io/reader (str postdir "post.md"))]
     (assoc (read-string (first (take 1 (line-seq f))))
       :url postdir)))
-      
+
 (defn extract-post-content
   "Returns, as a string of text, the content of a post.md,
    given its directory"
@@ -31,3 +32,22 @@
   (spit (str postdir "post.html")
         (r/markdown (extract-post-content postdir))))
 
+(defn process-post-meta
+  "Takes the post meta and adds to it as necessary.
+
+   Currently, only adds date if needed, or else
+   formats it correctly."
+  [m]
+  (assoc m :date (d/fulldate-string
+                  (d/date-from-string (m :date)))))
+
+(defn replace-post-meta
+  "Replaces the metadata map of the post.md in postdir
+   with 'm."
+  [postdir m]
+  (let [content (extract-post-content postdir)
+        newpost (str m \newline content)]
+    (spit (str postdir "post.md") newpost)))
+    
+    
+  
