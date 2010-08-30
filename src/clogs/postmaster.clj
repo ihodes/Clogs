06853ae9416d0776f1d2b2c770c116f4e8f5c207
Isaac Hodes <ihodes@mac.com>
2010-08-30 15:37:18 -0400

    fixed my add-to-postmaster fn

--- src/clogs/postmaster.clj
+++ src/clogs/postmaster.clj
@@ -1,5 +1,5 @@
 (ns clogs.postmaster
-  (:use [clojure.contrib.io :only (append-spit)]
+  (:use [clojure.java.io :as io]
         [clojure.java.io :only (reader)]))
 
 (def *postbox* "resources/postbox.txt")
@@ -7,10 +7,12 @@
 (defn add-to-postbox
   "Adds 'm to the end of the postbox."
   [m]
-  (append-spit *postbox* (str \newline m)))
+  (with-open [w (io/writer (io/file *postbox*)
+                           :append true)]
+    (spit w (str \newline m))))
 
 ;; lazy seq of post-maps in the postbox
 (def posts
-     (map #(do (println "hey") (read-string %)) (line-seq (reader *postbox*))))
+     (map read-string (line-seq (reader *postbox*))))
 
 ;; later: (find-post :date  "2010") (all posts with 2010 in the date) etc
