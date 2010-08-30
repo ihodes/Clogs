cda158f6d7e4230b387b27363eef9f6281733978
Isaac Hodes <ihodes@mac.com>
2010-08-30 17:44:56 -0400

    updated clj-time library

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "A static, control-freak's blogging engine, written in Clojure."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [clj-time "0.1.0-SNAPSHOT"]
+                 [clj-time "0.1.0-RC1"]
                  [enlive "1.0.0-SNAPSHOT"]
                  [org.clojars.nakkaya/markdownj "1.0.2b4"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
