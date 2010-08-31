525fe9d4fb68bde4a400a117d71258916fe46fd3
Isaac Hodes <ihodes@mac.com>
2010-08-31 01:16:46 -0400

    Fixed some path issues

--- src/clogs/core.clj
+++ src/clogs/core.clj
@@ -4,12 +4,17 @@
             [clogs.render :as r]
             [clogs.postmaster :as pm]))
 
+;; for testing
+(def ps ["p/initial-thoughts-on-clojure-mostly-awesome/" "p/e-and-clojure/" "p/new-blog/" "p/clojurists-guide-to-java/" "p/the-golden-ratio-is-evil/"])
+
 (def *posts-to-show* 5)
 
 (def *index* "resources/index.html")
 (def *archives* "resources/archives.html")
 (def *feed* "resources/feed.xml")
 
+;; postdir = p/name-of-post-directory/
+  
 (defn pre-publish-post
   "Processes the post.md in 'postdir, adding the metadata to the postbox,
    creating the index.html for the post and dating the post."
@@ -20,7 +25,7 @@
                :content (r/markdown rawcontent))]
     (p/replace-post-meta postdir meta)
     (pm/add-to-postbox meta)
-    (spit (str postdir "index.html")
+    (spit (str (p/full-post-dir postdir) "index.html")
           (r/concat-strings
            (r/base-render {:title (post :title)
                            :body (r/just-post-snippet post)})))))
@@ -41,7 +46,7 @@
   ([n] (spit *feed*
              (r/concat-strings
               (r/rss-render
-               (map p/assoc-escaped-content (reverse (take-last n (pm/posts)))))))))
+               (map p/assoc-content (reverse (take-last n (pm/posts)))))))))
 
 (defn build-archives
   []
@@ -51,6 +56,7 @@
           {:title "The Archives"
            :body (r/archive-snippet (reverse (pm/posts)))}))))
 
+;; BUGGY (see comment in fn body)
 (defn- prepend-to-archives
   "Prepends a formatted archive snippet string to  *archives*.
 
@@ -61,13 +67,13 @@
          (html/emit*
           (html/at (first (html/html-resource "archives.html")) 
                    [[:article (html/nth-of-type 1)]]
-                   (html/before
+                   (html/before ;; need a version of this that doesn't escape the HTML
                     (str \newline s \newline)))))))
 
 (defn post-to-archive
   "Prepends the specified post to the archives."
   [postdir]
-  (let [meta (p/process-post-meta (p/extract-post-meta postdir))
+  (let [meta (p/process-post-meta (p/extract-post-meta postdir) postdir)
         rawcontent (p/extract-post-content postdir)
         post (assoc meta :content (r/markdown rawcontent))]
     (prepend-to-archives
@@ -77,6 +83,7 @@
   "Publish post to index, archives and rss, as well as create the permalink."
   [postdir]
   (pre-publish-post postdir)
-  (post-to-archive postdir)
+  (build-archives) ;; for now...
+  ;;(post-to-archive postdir)
   (build-index *posts-to-show*)
   (build-rss *posts-to-show*))
